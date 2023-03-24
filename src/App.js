40aa9f8d9429ca7a8e2159e099d2e1df9f7f8f7f
Agustin Dietz <agustintomasdietz@gmail.com>
2023-03-23 23:09:08 -0300

    Agregando compronentes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+
+  const numero = 1
+
+  return (
+      <NavBar number = {numero}/>
+  );
+}
+
+export default App;
